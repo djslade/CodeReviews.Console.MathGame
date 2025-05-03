1f12c46f69b16aa898cda6eccf633eff76b54964
fenroe <scrambles9494@gmail.com>
2025-05-03 23:45:35 +0300

    removed issue highlighted in lint pipeline

--- mathGame.djslade/lib/game/Game.cs
+++ mathGame.djslade/lib/game/Game.cs
@@ -71,7 +71,7 @@ namespace mathGame.djslade.lib.game
                 }
             });
 
-            Task _ = Task.Run(async () =>
+            Task.Run(async () =>
             {
                 for (int i = 15; i > 0; i--)
                 {
